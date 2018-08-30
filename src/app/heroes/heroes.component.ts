8bcd795deb53796cb46f5be6c18822c8ebc3a251
fengzhibushang1101 <fengzhibushang@163.com>
2018-08-30 09:41:23 +0800

    feature: complete http module
    Change-Id: I6ac2bb155d7fbdaf1bb9a799237093a4296773cc

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,13 +12,20 @@ export class HeroesComponent implements OnInit {
     heroes: Hero[];
     constructor(private heroService: HeroService) {
     }
-    ngOnInit() {
+
+    ngOnInit () {
         this.getHeroes();
     }
-    getHeroes(): void {
+
+    getHeroes (): void {
         this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
     }
-    add(name: string): void {
+
+    delete (hero: Hero) {
+        this.heroService.deleteHero(hero).subscribe();
+        this.heroes = this.heroes.filter(h => h !== hero);
+    }
+    add (name: string): void {
         name = name.trim();
         if (!name) { return; }
         this.heroService.addHero({ name } as Hero).subscribe(hero => {
